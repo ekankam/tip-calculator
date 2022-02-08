ef2166ef7515e4318161a8aace0b2b6cfe86620a
ekankam <eb.kankam26@gmail.com>
2022-02-08 13:34:22 -0600

    Updated the README file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Footer from './components/Footer'
 import TipBox from './components/TipBox'
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
             </h2>
 
             <TipBox />
+            <Footer />
         </div>
     )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="attribution">
+                Challenge by{' '}
+                <a
+                    href="https://www.frontendmentor.io?ref=challenge"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    Frontend Mentor
+                </a>
+                . Coded by{' '}
+                <a
+                    href="https://github.com/ekankam/tip-calculator"
+                    rel="noreferrer"
+                    target="_blank"
+                >
+                    Ebenezer Kankam
+                </a>
+                .
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
